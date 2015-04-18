b9114bb8824c116835886064bd0b6e2fab9ccced
nalabjp <nalabjp@gmail.com>
2015-04-19 04:01:54 +0900

    Enable active_job with sidekiq

--- config/puma.rb
+++ config/puma.rb
@@ -8,8 +8,28 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+@sidekiq_pid = nil
+
+def sidekiq_boot?
+  by_foreman = begin
+    IO.foreach(Rails.root.join('Procfile')).any?{|line| line.include?('sidekiq')}
+  rescue
+    false
+  end
+  File.exists?(Rails.root.join('config/sidekiq.yml')) && by_foreman.!
+end
+
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
+
+  if sidekiq_boot?
+    @sidekiq_pid ||= spawn('bundle exec sidekiq -C config/sidekiq.yml')
+    Rails.logger.info("Spawned sidekiq #{@sidekiq_pid}")
+  end
+end
+
+on_worker_shutdown do
+  Process.kill('kill', @sidekiq_pid) if @sidekiq_pid
 end

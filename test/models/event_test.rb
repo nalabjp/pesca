0021d071201115a2b3eef3ea4e431e910f95c34f
nalabjp <nalabjp@gmail.com>
2015-05-26 17:38:40 +0900

    Add annotation for `Event`

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  provider    :string(32)       not null
+#  event_id    :string(16)       not null
+#  title       :string           not null
+#  description :text
+#  catch       :text
+#  address     :string
+#  event_url   :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  unique_on_provider_and_event_id  (provider,event_id) UNIQUE
+#
+
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
